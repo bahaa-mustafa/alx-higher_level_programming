2f43d5a7d862556bba0b1f8ccb76655889a1d992
Bahaa Badr <bahaamustafabadr@gmail.com>
2024-02-04 03:47:49 -0800

    update

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,12 +3,11 @@
 # if it found pass else return false
 
 def islower(c):
-    if ord('{}'.format(c)) > 96 and ord('{}'.format(c)) < 123:
-        return True
-
-    elif not isinstance(c, chr):
+    if (c, chr):
+            if ord('{}'.format(c)) > 96 and ord('{}'.format(c)) < 123:
+                return True
+            else:
+                return False
+    else:
         raise TypeError()
-    else: #ord('{}'.format(c)) > 64 and ord('{}'.format(c)) < 91:
-        return False
-#    else:
-#        raise TypeError()
+
