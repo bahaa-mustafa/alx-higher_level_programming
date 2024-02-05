dbf7508ee8238d9fad0851ed7c5bcef66b05682f
Bahaa Badr <bahaamustafabadr@gmail.com>
2024-02-05 12:24:48 -0800

    update task 6

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,7 +1,2 @@
-class NameError(Exception):
-    def __init__(self, message=""):
-        super().__init__(message)
-
-
 def raise_exception_msg(message=""):
-    raise NameError(message)
+    raise NameError(message="")
