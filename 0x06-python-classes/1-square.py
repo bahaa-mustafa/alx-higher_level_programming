d75313fcb65d1e44d12f599dfa11133e4c157b83
Bahaa Badr <bahaamustafabadr@gmail.com>
2024-02-06 07:40:42 -0800

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+"""square modual."""
 
+
+class Square:
+    """define a class."""
+    def __init__(self, size):
+        """define function with instance attribute"""
+        self.__size = size
