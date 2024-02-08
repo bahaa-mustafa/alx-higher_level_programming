6a9d5a39733b38ad4a2f16a3ba4f5e805450af07
Bahaa Badr <bahaamustafabadr@gmail.com>
2024-02-08 06:27:09 -0800

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 import add_0
 
-def print_add():
-    a = 1
-    b = 2
-
-    print("{} + {} = {}".format(a, b, add(a, b)))
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add(a, b)))
 
 
