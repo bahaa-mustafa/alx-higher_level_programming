cd27e6eaec61c3e87b1310142b5142b7b97831c1
Bahaa Badr <bahaamustafabadr@gmail.com>
2024-02-12 13:52:33 -0800

    task 0

--- 0x07-python-test_driven_development/temp.py
+++ 0x07-python-test_driven_development/temp.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
 """function module."""
 
+
 def add_integer(a, b=98):
     """add function.
+
+    Args:
+	a: frist number integer or float
+	b: second number integer or float
+
     Raise:
 	TypeError: if a and b not integer or float
+
+    Return:
+	add of a and b
     """
     if type(a) not in(int, float):
         raise TypeError("a must be an integer")
+
     if type(b) not in(int, float):
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
+
+
 if __name__ == "__main__":
     import doctest
     doctest.testfile("tests/0-add_integer.txt")
+
