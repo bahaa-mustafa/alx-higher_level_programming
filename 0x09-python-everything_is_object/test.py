1a3ec1f17b0f236c56078d0677cfe9da98f11db7
Bahaa Badr <bahaamustafabadr@gmail.com>
2024-02-13 10:50:59 -0800

    answers

--- 0x09-python-everything_is_object/test.py
+++ 0x09-python-everything_is_object/test.py
@@ -21,3 +21,17 @@ l1 = [1, 2, 3]
 l2 = [4, 5, 6]
 assign_value(l1, l2)
 print(l1)
+
+print("---------")
+
+a = [1, 2, 3, 4]
+print(id(a))
+a = a + [5]
+print(id(a))
+
+print("---------")
+
+a = [1, 2, 3]
+print(id (a))
+a += [4]
+print(id(a))
