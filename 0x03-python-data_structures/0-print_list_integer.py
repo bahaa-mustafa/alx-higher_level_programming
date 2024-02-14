d42c67aec8f45994a78cc6d516e139008cf3d5ca
Bahaa Badr <bahaamustafabadr@gmail.com>
2024-02-14 08:37:35 -0800

    task 0

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function module"""
+
+
+def print_list_integer(my_list=[]):
+    """print all integer in my_list[]"""
+    for item in my_list:
+        print("{}".format(item))
