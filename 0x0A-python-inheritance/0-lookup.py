17f571c621c1bcd279292f3e10be60f7145cddfd
Bahaa Badr <bahaamustafabadr@gmail.com>
2024-02-19 07:01:45 -0800

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""function module"""
+
+
+def lookup(obj):
+    """ function that returns the list of available attributes and 
+    ethods of an object.
+
+    Args:
+        obj: class object
+
+    Return:
+        available attributes
+    """
+    if not isinstance(obj, object):
+        return None
+    return dir(obj)
