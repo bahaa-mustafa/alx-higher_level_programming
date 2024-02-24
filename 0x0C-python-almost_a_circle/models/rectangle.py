41d0e4283659a1ebe0c662e42d2f337556e3a02d
Bahaa Badr <bahaamustafabadr@gmail.com>
2024-02-24 04:52:44 -0800

    task 3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -30,31 +30,44 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        self.check_value("width", value)
         self.__width = value
 
     @property
     def height(self):
-        """getter and setter for reactangle width"""
+        """getter and setter for reactangle height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        self.check_value("height", value)
         self.__height = value
 
     @property
     def x(self):
-        """getter and setter for reactangle width"""
+        """getter and setter for reactangle x"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        self.check_value("x", value, False)
         self.__x = value
 
     @property
     def y(self):
-        """getter and setter for reactangle width"""
+        """getter and setter for reactangle y"""
         return self.__y
 
     @y.setter
     def y(self, value):
+        self.check_value("y", value, False)
         self.__y = value
+
+    def check_value(self, name, value, not_zero = True):
+        """check if value is number integer bigger than 0 or equal"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value < 0 and not_zero:
+            raise ValueError("{} must be > 0".format(name))
+        elif value <= 0 and not_zero == False:
+            raise ValueError("{} must be >= 0".format(name))
