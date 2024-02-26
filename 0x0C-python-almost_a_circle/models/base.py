f49161ca11897eefb7a2b27c0080ce8bdc00ff06
Bahaa Badr <bahaamustafabadr@gmail.com>
2024-02-26 13:17:50 -0800

    task 14

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,3 +14,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """confarm json string"""
+        import json
+
+
+        if list_dictionaries:
+            return json.dumps(list_dictionaries)
+        return "[]"
