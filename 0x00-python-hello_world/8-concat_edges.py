a789133d59117e5586559a1073d6f6efdd9668c3
bahaa_mustafa <bahaamustafabadr@gmail.com>
2024-04-11 21:37:08 +0200

    update

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-str = str.split(" ")
-str = (str[5]+" "+ str[6]+" "+str[12]+ " "+str[0])
+str = str[39:66] + str[106:112] + str[0:6]
 print(str)
