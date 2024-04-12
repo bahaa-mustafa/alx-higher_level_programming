cab4879f805126a8fd0fb1c9faf702cdb2d26d94
bahaa_mustafa <bahaamustafabadr@gmail.com>
2024-04-12 05:52:58 +0200

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
 
-def islower(c):
-    if ord(c) >= ord('a') and ord(c) <= ord('z'):
-        return True
-    else:
-        return False
-
-# calculat length of string
-# make loop on sring
-# check if char. is upper ==> pass
-# if not convert to upper by using ascii code: (ascii code - 32)
-
 def uppercase(str):
     for c in str:
-        print("{:c}"
-                .format(ord(c)) if not islower(c) else (ord(c) - 32),end="")
+        if ord(c) >= 65 and ord(c) <= 90:
+            print("{}".format(c), end="")
+        elif ord(c) >= 97 and ord(c) <= 122:
+            print("{}".format(chr(ord(c) - 32)), end="")
+        else:
+            print("{}".format(c), end="")
     print("")
