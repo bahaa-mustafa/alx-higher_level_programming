9794d1771c862fa1f8258b763b4d7df339a1a7c9
bahaa_mustafa <bahaamustafabadr@gmail.com>
2024-04-12 06:22:22 +0200

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+
 def convert_lower(c):
+    
     if ord(c) >= 97 and ord(c) <= 122:
         return chr(ord(c) - 32)
     else:
         return c
 
 def uppercase(str):
+
     for c in str:
         print("{}".format(convert_lower(c)), end="")
     print("")
