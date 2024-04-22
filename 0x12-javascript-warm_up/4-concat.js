1dd5d697766f6d6f2e4a5ff0d44d61d8f9908891
bahaa_mustafa <bahaamustafabadr@gmail.com>
2024-04-22 22:26:40 +0200

    task 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-if (process.argv[2]) {
-  console.log(process.argv[2]);
+if (process.argv[2] && process.argv[3]) {
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
+} else if (process.argv[2]) {
+  console.log(process.argv[2] + ' is undefined');
 } else {
-  console.log('No argument');
+  console.log('undefined is undefined');
 }
