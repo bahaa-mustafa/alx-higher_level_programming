02483a475ef91ab26679774d212ca4650c3c85ab
bahaa_mustafa <bahaamustafabadr@gmail.com>
2024-04-22 22:12:37 +0200

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-const args = process.argv;
-if (args.length === 3){
-    console.log('Argument found'); 
-}
-else if (args.length > 3){
-    console.log('Arguments found');
-}
-else{
-    console.log('No argument');
+if (process.argv.length === 3) {
+  console.log('Argument found');
+} else if (process.argv.length > 3) {
+  console.log('Arguments found');
+} else {
+  console.log('No argument');
 }
