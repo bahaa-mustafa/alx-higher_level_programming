817d47aebeb838f4e3bb725198d7a79c78e6cda3
bahaa_mustafa <bahaamustafabadr@gmail.com>
2024-04-24 16:49:05 +0200

    task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,6 +3,6 @@ if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
-    console.log('x'.repeat(parseInt(process.argv[2])));
+    console.log('X'.repeat(parseInt(process.argv[2])));
   }
 }
