0f09356b8ee4b35591e9bb968c46393179bca223
bahaa_mustafa <bahaamustafabadr@gmail.com>
2024-05-06 00:43:28 +0300

    task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all records of the table second_table
-SELECT score, name FROM second_table ORDER BY score DESC WHERE name IS NOT NULL;
+SELECT score, name FROM second_table ORDER BY score DESC WHERE name != NULL;
