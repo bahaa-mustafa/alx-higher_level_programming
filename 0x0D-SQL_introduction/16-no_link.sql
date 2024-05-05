cc1782914d19f463649fb7265da4f0dd4f33af0e
bahaa_mustafa <bahaamustafabadr@gmail.com>
2024-05-06 00:40:01 +0300

    task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all records of the table second_table
-SELECT score, name FROM second_table IF name EXISTS ORDER BY score DESC;
+SELECT score, name FROM second_table HAVING name ORDER BY score DESC;
