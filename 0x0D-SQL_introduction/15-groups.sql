6313f148c6c6136916bb9f529a29d24313dce054
bahaa_mustafa <bahaamustafabadr@gmail.com>
2024-05-06 00:30:27 +0300

    task 15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lists the number of records with the same score
-SELECT score, name FROM second_table GROUP BY score HAVING COUNT(*) > 1 ORDER BY score DESC;
+SELECT score, name FROM second_table HAVING COUNT(score) > 1 ORDER BY score DESC;
